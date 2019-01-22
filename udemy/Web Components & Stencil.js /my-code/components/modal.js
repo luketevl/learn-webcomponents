f355c58850f8beb275fe81ea371a93ef72719a80
luketevl <luketevl@gmail.com>
2019-01-22 17:54:24 -0200

    finish course

--- udemy/Web Components & Stencil.js /my-code/components/modal.js
+++ udemy/Web Components & Stencil.js /my-code/components/modal.js
@@ -20,6 +20,14 @@ class Modal extends HTMLElement{
         opacity: 0;
         pointer-events: none;
       }
+      #modal, #backdrop{
+        opacity: 0;
+        pointer-events: none;
+      }
+      :host([opened]) #modal, :host([opened]) #backdrop{
+        opacity: 1;
+        pointer-events: all;
+      }
       #modal{
         position: fixed;
         top: 15vh;
@@ -59,26 +67,70 @@ class Modal extends HTMLElement{
       <div id="backdrop"></div>
       <div id="modal">
         <header>
+          <slot name="title"></slot>
           <h1>Please confirm</h1>
         </header>
         <section id="main">
           <slot></slot>
         </section>
         <section id="actions">
-          <button>Cancel</button>
-          <button>Yeah !</button>
+          <slot name="action">
+            <button id="cancel">Cancel</button>
+            <button id="confirm">Yeah !</button>
+          </slot>
         </section>
       
       </div>
-    `
+    `;
+
+    const slots = this.shadowRoot.querySelectorAll('slot');
+    slots[1].addEventListener('slotchange', event => {
+      console.dir(slots[1].assignedNodes());
+    });
+
+    const backdrop = this.shadowRoot.querySelector('#backdrop');
+    const cancelButton  = this.shadowRoot.querySelector('#cancel');
+    const confirmButton = this.shadowRoot.querySelector('#confirm');
+
+    cancelButton.addEventListener('click', this._cancel.bind(this))
+    confirmButton.addEventListener('click', this._confirm.bind(this))
+    backdrop.addEventListener('click', this._cancel.bind(this));
+
   }
-  static get observedAttributes(){
-    return ['opened']
+  // static get observedAttributes(){
+  //   return ['opened']
+  // }
+
+  // attributeChangedCallback(name, oldValue, newValue){
+  //   if(name === 'opened'){
+  //     if(this.hasAttribute('opened')){
+  //       this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+  //       this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+  //       this.shadowRoot.querySelector('#modal').style.opacity = 1;
+  //       this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+  //     }
+  //   }
+  // }
+
+  open(){
+    this.setAttribute('opened', '');
+  }
+  hide(){
+    this.removeAttribute('opened');
   }
 
-  attributeChangedCallback(name, oldValue, newValue){
-    if(name === 'opened'){}
-    console.log(name);
+  _cancel(event){
+    this.hide();
+    const cancelCustom = new Event('cancel',{
+      bubbles: true,
+      composed: true
+    });
+    event.target.dispatchEvent(cancelCustom);
+  }
+  _confirm(){
+    this.hide();
+    const confirmEvent = new Event('confirm')
+    this.dispatchEvent(confirmEvent);
   }
 }
 
